546a1c2546fc2405e036c019e2a830917e4312a2
Yankı Ekin Yüksel <yanki@aposto.com>
2024-09-08 22:16:56 +0300

    init

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script>
+import Navbar from './components/Navbar.vue';
+</script>
+
+<template>
+  <section>
+    <Navbar language="en" />
+    <NuxtPage />
+  </section>
+</template>
---
+++ components/Navbar.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+const props = defineProps(['language'])
+
+const dict = computed(() => {
+	return {
+		en: {
+			home: 'Home',
+			about: 'About',
+			contact: 'Contact'
+		},
+		fr: {
+			home: 'Accueil',
+			about: 'À propos',
+			contact: 'Contact'
+		}
+	}
+})
+</script>
+
+<template>
+	<nav>
+		<ul>
+			<li
+				v-for="(value, key) in dict[props.language as unknown as 'en' | 'fr']"
+			></li>
+		</ul>
+	</nav>
+</template>
