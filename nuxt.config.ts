53f511eebc9adf1d72445ea9ffeb03babe98d693
Yankı Ekin Yüksel <yanki@aposto.com>
2024-09-17 22:41:22 +0300

    fix: Remove nuxt image module and add locales

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/image'],
+  modules: ['@nuxt/content'],
   routeRules: {
     '/': { prerender: true }
+  },
+  content: {
+    defaultLocale: 'fr-FR',
+    locales: ['fr-FR', 'en-US'],
   }
 })
