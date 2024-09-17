7fa0deb67b0e776dcb4220f7120ce19100030351
Yankı Ekin Yüksel <yanki@aposto.com>
2024-09-17 23:00:25 +0300

    chore: Disable nuxt/image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/image'],
   routeRules: {
     '/': { prerender: true }
   },
+  modules: ['@nuxt/content'],
   content: {
     defaultLocale: 'fr-FR',
     locales: ['fr-FR', 'en-US'],
   },
-  image: {
-    dir: 'public/img'
-  }
 })
