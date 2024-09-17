d99bd502fe235ea0c2b529eda422f86d9c2ccdd9
Yankı Ekin Yüksel <yanki@aposto.com>
2024-09-17 22:58:49 +0300

    chore: Re-enable nuxt/image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content'],
+  modules: ['@nuxt/content', '@nuxt/image'],
   routeRules: {
     '/': { prerender: true }
   },
   content: {
     defaultLocale: 'fr-FR',
     locales: ['fr-FR', 'en-US'],
+  },
+  image: {
+    dir: 'public/img'
   }
 })
